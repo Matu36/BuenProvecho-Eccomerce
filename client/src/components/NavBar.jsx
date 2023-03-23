aec174b48165b38ce9371b2fd6189e840e450db4
Matu36 <matipineda85@live.com.ar>
2023-03-23 14:09:23 -0300

    estilos

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import DarkMode from "../utils/DarkMode";
-import { FaInstagram, FaFacebook, FaMapMarkerAlt, FaWhatsapp   } from "react-icons/fa"
+import { FaInstagram, FaFacebook, FaMapMarkerAlt, FaWhatsapp  } from "react-icons/fa"
+import { GiChickenOven, GiFishEggs, GiFrenchFries, GiFullPizza}  from "react-icons/gi";
+import {TbSalad, TbIceCream} from "react-icons/tb";
+import {BiDrink, BiDish} from "react-icons/bi";
 import {
-  Text,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
+  useColorMode,
   Box,
   Flex,
   IconButton,
@@ -18,16 +16,34 @@ import {
   DrawerCloseButton,
   DrawerHeader,
   DrawerBody,
+  Button,
+  
 } from "@chakra-ui/react";
-import { HamburgerIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import SearchBar from "./SearchBar";
 
 export default function NavBar({ handleSelectFood }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
+  const {colorMode} = useColorMode ();
+
+  const buttonColorScheme = colorMode === "light" ? "#F08080" : "gray";
+
   return (
     <Box>
-      <IconButton
+      
+
+      <Box>
+        <Flex
+          bg="blue.500"
+          color="white"
+          py={{ base: 2, md: 4 }}
+          px={8}
+          display={{ base: "flex", md: "flex" }}
+          justifyContent={{ base: "space-between", md: "flex-start" }}
+        >
+          <Box>
+          <IconButton
         aria-label="Abrir menú"
         icon={<HamburgerIcon />}
         size="md"
@@ -39,43 +55,44 @@ export default function NavBar({ handleSelectFood }) {
 
       <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
         <DrawerOverlay />
-        <DrawerContent bg="gray.800" p="4">
+        <DrawerContent bg="gray.800" p="9" size="xs">
           <DrawerCloseButton />
-          <DrawerHeader color="white">Menú</DrawerHeader>
-          <DrawerBody>{/* Agrega aquí los elementos del menú */}</DrawerBody>
+          <DrawerHeader color="white">Categorías</DrawerHeader>
+          <DrawerBody>
+            <Button variant="ghost" color="white" leftIcon={<GiChickenOven />}>
+    Carnes
+  </Button>
+  <Button variant="ghost" color="white" leftIcon={<GiFullPizza />}>
+    Pastas
+  </Button>
+  <Button variant="ghost" color="white" leftIcon={<GiFishEggs />}>
+    Pescados
+  </Button>
+  <Button variant="ghost" color="white" leftIcon={<TbSalad />}>
+    Ensaladas
+  </Button>
+  <Button variant="ghost" color="white" leftIcon={<GiFrenchFries />}>
+    Minutas
+  </Button>
+  <Button variant="ghost" color="white" leftIcon={<BiDish />}>
+    Platos Frios
+  </Button>
+  <Button variant="ghost" color="white" leftIcon={<BiDrink />}>
+    Bebidas
+  </Button>
+  <Button variant="ghost" color="white" leftIcon={<TbIceCream />}>
+    Postres
+  </Button></DrawerBody>
         </DrawerContent>
       </Drawer>
-
-      <Box>
-        <Flex
-          bg="blue.500"
-          color="white"
-          py={4}
-          px={8}
-          display={{ base: "none", md: "flex" }}
-        >
-          <Box>
           <DarkMode />
           </Box>
-          <Box flex="1" ml="auto" mr="auto" maxWidth="500px">
+          <Box flex="1" ml= "auto" mr="auto" maxWidth="500px">
           <SearchBar handleSelectFood= {handleSelectFood} />
           </Box>
 
           <Box display="flex" alignItems="center" mr={4}>
-            <Menu>
-              <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                Categorias
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Carnes</MenuItem>
-                <MenuItem>Pastas</MenuItem>
-                <MenuItem>Pescados</MenuItem>
-                <MenuItem>Ensaladas</MenuItem>
-                <MenuItem>Minutas</MenuItem>
-                <MenuItem>Platos Frios</MenuItem>
-                <MenuItem>Bebidas</MenuItem>
-              </MenuList>
-            </Menu>
+            
 
             <Box display="flex" alignItems="center">
   <IconButton
@@ -85,6 +102,9 @@ export default function NavBar({ handleSelectFood }) {
     aria-label="Instagram"
     icon={<FaInstagram />}
     mr={2}
+    colorScheme={buttonColorScheme}
+    
+    
   />
   <IconButton
     as="a"
@@ -93,6 +113,7 @@ export default function NavBar({ handleSelectFood }) {
     aria-label="Facebook"
     icon={<FaFacebook />}
     mr={2}
+    colorScheme={buttonColorScheme}
   />
   <IconButton
     as="a"
@@ -101,6 +122,7 @@ export default function NavBar({ handleSelectFood }) {
     aria-label="Geolocalización"
     icon={<FaMapMarkerAlt />}
     mr={2}
+    colorScheme={buttonColorScheme}
   />
   <IconButton
     as="a"
@@ -108,11 +130,13 @@ export default function NavBar({ handleSelectFood }) {
     target="_blank"
     aria-label="Whatsapp"
     icon={<FaWhatsapp />}
+    colorScheme={buttonColorScheme}
   />
 </Box>
             
           </Box>
         </Flex>
+        
       </Box>
     </Box>
   );
