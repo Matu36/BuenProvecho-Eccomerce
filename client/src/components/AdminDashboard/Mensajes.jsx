0d1d9330ee9397d5ee82aeeab144376521039b8e
Matu36 <matipineda85@live.com.ar>
2023-04-17 02:28:07 -0300

    notificacionesmensajesterminada

--- client/src/components/AdminDashboard/Mensajes.jsx
+++ client/src/components/AdminDashboard/Mensajes.jsx
@@ -1,26 +1,30 @@
-import React, {useEffect, useState} from "react";
-import {useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Paginacion from "./Paginación";
-import { Box, Text, Divider } from "@chakra-ui/react";
+import { Box, Text, Divider, Button, Flex } from "@chakra-ui/react";
+import { REMOVE_ONE_FROM_MESSAGE } from "../../Redux/actions";
 
-export default function Mensajes () {
-    
-    const mensajes = useSelector((state) => state.mensajes);
+export default function Mensajes() {
+  const dispatch = useDispatch();
 
-    const message = mensajes.map((product) => {
-        return {
-         
-          email: product.email,
-          Nombre: product.Nombre,
-          Mensaje: product.Mensaje
-          
-        };
-      });
-      
+  const mensajes = useSelector((state) => state.mensajes);
 
-    const [ingredients, setIngredients] = useState(message);
+  const marcarComoLeido = (id) => {
+    dispatch({ type: REMOVE_ONE_FROM_MESSAGE, payload: id });
+  };
 
-    //PAGINADO
+  const message = mensajes.map((product) => {
+    return {
+      id: product.id,
+      email: product.email,
+      Nombre: product.Nombre,
+      Mensaje: product.Mensaje,
+    };
+  });
+
+  const [ingredients, setIngredients] = useState(message);
+
+  //PAGINADO
 
   const [currentPage, setCurrentPage] = useState(1); //Pagina Actual seteada en 1
   const [numberOfPage, setNumberOfPage] = useState(0); //Numero de Paginas seteado en 0
@@ -42,27 +46,48 @@ export default function Mensajes () {
 
   //FIN PAGINADO
 
-    return (
-        <Box bg="gray.100" p={4} borderRadius="lg">
-        {totalIngredients.map((msg, index) => (
-          <Box key={index} mb={4}>
-          <Text fontWeight="bold">{msg.Nombre}</Text>
-          <Text>{msg.email}</Text>
-          <Text>{msg.Mensaje}</Text>
+  const [clickedButton, setClickedButton] = useState(null);
+  const [buttonState, setButtonState] = useState({});
+
+  return (
+    <Box bg="gray.100" p={4} borderRadius="lg">
+      {totalIngredients.map((msg, index) => (
+        <Box key={index} mb={4}>
+          <Flex justify="space-between" align="center">
+            <Box>
+              <Text fontWeight="bold">{msg.Nombre}</Text>
+              <Text>{msg.email}</Text>
+              <Text>{msg.Mensaje}</Text>
+            </Box>
+            <Button
+              colorScheme="green"
+              size="sm"
+              variant={buttonState[msg.id] ? "solid" : "outline"}
+              isDisabled={buttonState[msg.id]}
+              onClick={() => {
+                setButtonState({ ...buttonState, [msg.id]: true });
+                setClickedButton(msg.id); // Actualizamos el estado local del botón que se ha clicado
+                marcarComoLeido(msg.id);
+                // Llamamos a la función marcarComoLeido para cambiar el estado global
+              }}
+            >
+              Marcar como leído
+            </Button>
+          </Flex>
           <Divider my={2} borderColor="gray.300" />
         </Box>
-        ))}
-    
-<Box width="100%" marginBottom="2rem">
-          <br />
-          {message && (
-            <Paginacion
-              currentPage={currentPage}
-              numberOfPage={numberOfPage}
-              handlePageNumber={handlePageNumber}
-            />
-          )}
-        </Box>
-        </Box>
-    )
+      ))}
+
+      <Box width="100%" marginBottom="2rem">
+        <br />
+        {message && (
+          <Paginacion
+            currentPage={currentPage}
+            numberOfPage={numberOfPage}
+            handlePageNumber={handlePageNumber}
+          />
+        )}
+      </Box>
+    </Box>
+  );
 }
