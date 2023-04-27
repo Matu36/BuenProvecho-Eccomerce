2544ecdd3da310df60ff55f975cfffd7ceb10791
Matu36 <matipineda85@live.com.ar>
2023-04-27 18:38:53 -0300

    botonlogin

--- api/src/controllers/UsersCont.js
+++ api/src/controllers/UsersCont.js
@@ -3,11 +3,11 @@ const { Users } = require("../db.js");
 
 const getUsers = async (req, res) => {
   try {
-    if (!req.query?.id || !req.query?.email) throw "No query params";
+    if (!req.query?.email) throw "No query params";
 
     console.log(req.query);
     let requestUser = await Users.findOne({
-      where: { id: req.query.id, email: req.query.email },
+      where: { email: req.query.email },
     });
 
     if (!requestUser) return res.status(403).send("Wrong user");
@@ -16,7 +16,7 @@ const getUsers = async (req, res) => {
 
     if (requestUser.dataValues.role !== null)
       returnedUsers = await Users.findAll();
-    else returnedUsers = await Users.findAll({ where: { id: req.query.id } });
+    else returnedUsers = await Users.findAll({ where: { email: req.query.email } });
 
     return !returnedUsers
       ? res.status(404).send("Users Not Found")
@@ -26,6 +26,7 @@ const getUsers = async (req, res) => {
     res.status(500).send(error);
   }
 };
+
 
 
 const postUser = async (req, res) => {
--- client/src/components/Auth0.jsx
+++ client/src/components/Auth0.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { RiUserFill } from "react-icons/ri";
-import { IconButton } from "@chakra-ui/react";
+import { IconButton,  MenuItem, 
+  Menu, MenuButton, MenuList} from "@chakra-ui/react";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { getUsers } from "../Redux/actions";
+import { useNavigate } from 'react-router-dom';
 
 
-const AuthButton = () => {
+  const AuthButton = () => {
   const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
-  /* const [isAdmin, setIsAdmin] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false); */
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
+  
 
-  //LOCALSTORAGE
+  //LOCALSTORAGE --> USO LA PROPIEDAD "user" DE 
+  //ESTA FORMA let currentUser = JSON.parse(localStorage.getItem("user")); PARA
+  //EL RESTO DE LOS COMPONENTES.
+  let currentUser = JSON.parse(localStorage.getItem("user"));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUsers(currentUser))
+    }, []);
+
+  const Usuario = useSelector (state => state.users);
+
 
   useEffect(() => {
     if (isAuthenticated && user) {
-      localStorage.setItem("user", JSON.stringify(user));
+      const {email } = user;
+      const userData = { email }; // utiliza sub como el id del usuario
+      localStorage.setItem("user", JSON.stringify(userData)); // almacenar los datos del usuario usando su sub como clave
     } else {
       localStorage.removeItem("user");
     }
-  }, [isAuthenticated, user]);
+}, [isAuthenticated, user]);
+
 
   // FIN LOCAL STORAGE
 
@@ -53,16 +73,65 @@ const AuthButton = () => {
 
   
 
+ 
+
   return (
-    <IconButton
-      icon={<RiUserFill />}
-      title={isAuthenticated ? user.email : "Iniciar sesión"}
-      aria-label={isAuthenticated ? "Cerrar sesión" : "Iniciar sesión"}
-      onClick={() => (isAuthenticated ? logout() : loginWithRedirect())}
-      bg={isAuthenticated ? "red.500" : "gray.500"}
-      _hover={{ bg: isAuthenticated ? "red.600" : "gray.600" }}
-    />
-  );
-};
+    <Menu isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)}>
+  <MenuButton
+    as={IconButton}
+    icon={<RiUserFill />}
+    title={isAuthenticated ? user.email : "Iniciar sesión"}
+    aria-label={isAuthenticated ? "Cerrar sesión" : "Iniciar sesión"}
+    bg={isAuthenticated ? "red.500" : "gray.500"}
+    _hover={{ bg: isAuthenticated ? "red.600" : "gray.600" }}
+    onMouseEnter={() => setIsMenuOpen(true)}
+    w="8"
+  />
+  {isMenuOpen && (
+    <MenuList maxW="xs">
+      <MenuItem
+        color="gray.600"
+        fontSize="md"
+        px="3"
+        py="2"
+        mx="auto"
+        textAlign="center"
+        onClick={() => {
+          if (isAuthenticated) {
+            logout();
+            window.location.reload();
+          } else {
+            loginWithRedirect();
+            window.location.reload();
+          }
+        }}
+        
+        
+      >
+        {isAuthenticated ? "Cerrar sesión" : "Iniciar sesión"}
+      </MenuItem>
+      {Usuario.length > 1 && (
+        <Link to="/admin">
+          <MenuItem
+            color="gray.600"
+            fontSize="md"
+            px="3"
+            py="2"
+            mx="auto"
+            textAlign="center"
+            
+          >
+            Admin
+          </MenuItem>
+        </Link>
+      )}
+    </MenuList>
+  )}
+</Menu>
+
+  )
+      }
+  
+    
 
 export default AuthButton;
