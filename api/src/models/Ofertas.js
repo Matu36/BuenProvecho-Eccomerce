361acd50506981fac1eae31d12f5fa6588deeb21
Matu36 <matipineda85@live.com.ar>
2023-05-21 00:31:11 -0300

    ofertas

--- api/src/models/Ofertas.js
+++ api/src/models/Ofertas.js
@@ -2,7 +2,7 @@ const { DataTypes } = require ('sequelize');
 
 module.exports = (sequelize) => {
 
-    sequelize.define('ofertas', {
+    sequelize.define('Ofertas', {
 
 Nombre: {
     type: DataTypes.STRING,
