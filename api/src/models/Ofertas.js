acb383e2e7b939bb142091c45d64cb92ffc79edb
Matu36 <matipineda85@live.com.ar>
2023-05-21 00:14:28 -0300

    model

--- api/src/models/Ofertas.js
+++ api/src/models/Ofertas.js
@@ -3,13 +3,7 @@ const { DataTypes } = require ('sequelize');
 module.exports = (sequelize) => {
 
     sequelize.define('ofertas', {
-id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primarykey:true,
-    autoIncrement:true
-    
-},
+
 Nombre: {
     type: DataTypes.STRING,
     allowNull:false,
