878fe8a793590751588c4494a261c9a132f9f0c1
Matu36 <matipineda85@live.com.ar>
2024-02-16 20:56:31 -0300

    h

--- api/src/db.js
+++ api/src/db.js
@@ -12,7 +12,6 @@ const { DB_URL, DB_DEPLOY, DATABASE_URL } = process.env;
 const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialect: "postgres",
 });
 
 const basename = path.basename(__filename);
