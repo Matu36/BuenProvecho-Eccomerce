c843193d75f29484a086402cd1e9a679d4cc3a27
Matu36 <matipineda85@live.com.ar>
2024-02-17 13:28:14 -0300

    login

--- client/src/components/AvisoLogin.jsx
+++ client/src/components/AvisoLogin.jsx
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from "react";
 import chef from "../img/CHEFICONO.png";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const AvisoLogin = () => {
-  const { user, isAuthenticated } = useAuth0();
+export default function AvisoLogin() {
+  const { user } = useAuth0();
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
     const hasBeenShown = localStorage.getItem("avisoShown");
 
-    if (isAuthenticated && !hasBeenShown) {
+    if (!hasBeenShown) {
       setVisible(true);
       localStorage.setItem("avisoShown", "true");
     }
-  }, [isAuthenticated]);
+  }, []);
 
   return visible ? (
     <div className="aviso-login show">
@@ -24,4 +24,4 @@ const AvisoLogin = () => {
       </div>
     </div>
   ) : null;
-};
+}
