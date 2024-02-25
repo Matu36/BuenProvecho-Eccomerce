fd8b1f509e1752c15f64c0dbc465fce3a28eb048
Matu36 <matipineda85@live.com.ar>
2024-02-25 00:48:44 -0300

    verga

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
-import React, { useEffect } from "react";
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import ShoppingCart from "../src/components/ShoppingCart/ShoppingCart/ShoppingCart";
@@ -10,9 +9,6 @@ import LoginUser from "../src/components/LoginUser";
 import Stripe from "../src/components/Stripe";
 import CheckoutMP from "./components/MercadoPago/CheckoutMP";
 import Carta from "./components/Carta";
-import { useAuth0 } from "@auth0/auth0-react";
-import { getUsers } from "./Redux/actions";
-import { useDispatch } from "react-redux";
 
 const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const auth0Client = process.env.REACT_APP_AUTH0_CLIENT;
@@ -26,43 +22,13 @@ export const Auth0ProviderConfig = {
 //https://buenprovecho.vercel.app/ => dominio
 
 function App() {
-  const location = useLocation();
-  const { isAuthenticated } = useAuth0();
-  const dispatch = useDispatch();
-  const userFromLogin = location?.state?.user; // Obtener el usuario desde la ubicación
-
-  useEffect(() => {
-    console.log("isAuthenticated:", isAuthenticated);
-    console.log("userFromLogin:", userFromLogin);
-    if (userFromLogin) {
-      dispatch(getUsers(userFromLogin));
-    }
-  }, [userFromLogin, dispatch, isAuthenticated]);
-
-  const email = userFromLogin?.email;
-
-  console.log("Usuario desde ubicación:", userFromLogin);
-  console.log("Email:", email);
-
-  console.log(userFromLogin);
-  console.log(email);
-
   return (
     <Auth0Provider {...Auth0ProviderConfig}>
       <Routes>
         <Route exact path={"/"} element={<Home />} />
         <Route exact path={"/"} element={<NavBar />} />
         <Route exact path={"/Scart"} element={<ShoppingCart />} />
-        <Route
-          path="/admin"
-          element={
-            userFromLogin?.email === "matipineda857@gmail.com" ? (
-              <AppAdmin />
-            ) : (
-              <Navigate to="/" />
-            )
-          }
-        />
+        <Route exact path={"/admin"} element={<AppAdmin />} />
         <Route exact path={"/Logued"} element={<LoginUser />} />
         <Route exact path={"/Checkout"} element={<Stripe />} />
         <Route exact path={"/CheckoutMP"} element={<CheckoutMP />} />
