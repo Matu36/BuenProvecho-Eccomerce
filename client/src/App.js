b9eda5177e77c87d80087b47c530ecad39d251a4
Matu36 <matipineda85@live.com.ar>
2024-02-25 01:42:29 -0300

    porfa

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import ShoppingCart from "../src/components/ShoppingCart/ShoppingCart/ShoppingCart";
 import AppAdmin from "./components/AdminDashboard/AppAdmin";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import LoginUser from "../src/components/LoginUser";
 import Stripe from "../src/components/Stripe";
 import CheckoutMP from "./components/MercadoPago/CheckoutMP";
 import Carta from "./components/Carta";
+import AdminRoute from "../src/components/AdminDashboard/AdminRoute";
 
 const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const auth0Client = process.env.REACT_APP_AUTH0_CLIENT;
@@ -22,13 +23,21 @@ export const Auth0ProviderConfig = {
 //https://buenprovecho.vercel.app/ => dominio
 
 function App() {
+  const { isAuthenticated, user } = useAuth0();
+
+  console.log(isAuthenticated);
+  console.log(user);
+  console.log(user.email);
   return (
     <Auth0Provider {...Auth0ProviderConfig}>
       <Routes>
         <Route exact path={"/"} element={<Home />} />
         <Route exact path={"/"} element={<NavBar />} />
         <Route exact path={"/Scart"} element={<ShoppingCart />} />
-        <Route exact path={"/admin"} element={<AppAdmin />} />
+        <Route
+          path={"/admin"}
+          element={<AdminRoute isAuthenticated={isAuthenticated} user={user} />}
+        />
         <Route exact path={"/Logued"} element={<LoginUser />} />
         <Route exact path={"/Checkout"} element={<Stripe />} />
         <Route exact path={"/CheckoutMP"} element={<CheckoutMP />} />
